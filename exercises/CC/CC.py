4cb9aea6fb72413148b5228bd64b58ab10062622
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-11-21 10:26:19 +0100

    TD4

--- exercises/CC/CC.py
+++ exercises/CC/CC.py
@@ -1,1 +1,23 @@
-print(1 + 1)
+def AfficheValeur(a):
+    """ Affiche la valeur de a en fonction de son argument """
+    if a < 0:
+        for i in range(3):
+            a -= 7
+            print(a)
+    else:
+        print(1)
+
+    pass
+
+
+help(AfficheValeur)
+
+
+def Coordonne(prenom, nom):
+    """ print des coordonnées """
+    nom = "Dupont"
+    ville = "Paris"
+
+    print(prenom + nom + " habite à ville " + ville)
+
+    pass
