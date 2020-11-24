c9850817905943e999587ef9af3888b9013a202d
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-11-24 23:59:47 +0100

    TD4

---
+++ exercises/TD04_listes/syracuse.py
@@ -0,0 +1,24 @@
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    liste = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = (n * 3) + 1
+        liste.append(n)
+    return liste
+
+
+print(syracuse(3))
+
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
+    for i in range(2, n_max + 1):
+        syracuse(i)
+
+    return True
+
+
+print(testeConjecture(10000))
