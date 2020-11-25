34044bb0ac7281dca795741d0a637a93fe795333
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-11-25 18:26:15 +0100

    TD4

--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -22,3 +22,53 @@ def testeConjecture(n_max):
 
 
 print(testeConjecture(10000))
+
+
+def tempsVol(n):
+    """ Retourne le temps de vol de n """
+    return len(syracuse(n))
+
+
+print("Le temps de vol de", 3, "est", tempsVol(3))
+
+
+def tempsVolListe(n_max):
+    """ Retourne la liste de tous les temps de vol de 1 à n_max """
+    liste = [1]
+    for i in range(2, n_max + 1):
+        liste.append(tempsVol(i))
+
+    return liste
+
+
+print(tempsVolListe(100))
+
+
+def VerTempsDeVol(n_max):
+    liste2 = []
+    liste = tempsVolListe(n_max)
+    liste2 = liste.copy()
+    liste2.sort()
+    rep = liste.index(liste2[-1]) + 1
+    return liste2[-1], rep
+
+
+print(VerTempsDeVol(10000))
+
+
+def syracuse2(n_max):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    liste2 = []
+    for i in range(1, n_max + 1):
+        liste = syracuse(i)
+        liste.sort()
+        liste2.append(liste[-1])
+    liste = liste2.copy()
+    liste2.sort()
+    rep = liste.index(liste2[-1]) + 1
+    return liste2[-1], rep
+
+
+print(syracuse2(10000))
+
+
