7b59be42cc16e22850dd0e900ed809ede839d452
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-12-02 16:06:23 +0100

    TD5

--- exercises/Avion.py
+++ exercises/Avion.py
@@ -2,24 +2,24 @@ route = [
     ["DSM", "ORD"],
     ["ORD", "BGI"],
     ["BGI", "LGA"],
-    
+
     ["SIN", "CDG"],
     ["CDG", "SIN"],
     ["CDG", "BUD"],
-    
+
     ["DEL", "DOH"],
     ["DEL", "CDG"],
     ["TLV", "DEL"],
-    
+
     ["EWR", "HND"],
     ["HND", "ICN"],
     ["HND", "JFK"],
-    
+
     ["ICN", "JFK"],
     ["JFK", "LGA"],
-    
+
     ["EYW", "LHR"],
-    
+
     ["LHR", "SFO"],
     ["SFO", "SAN"],
     ["SFO", "DSM"],
@@ -42,5 +42,3 @@ for i in route:
         possibleroute.append(i)
 
 print(possibleroute)
-
-
