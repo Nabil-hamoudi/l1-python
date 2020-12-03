b8d8100e10df49f27e45e95f078ea737f06ccfc5
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-12-03 15:00:02 +0100

    TD5

--- exercises/TD05_gui/cible.py
+++ exercises/TD05_gui/cible.py
@@ -5,7 +5,6 @@ color = ["blue", "green", "black", "yellow", "magenta", "red"]
 nbr = int(input("choississez le nombre de cercle"))
 
 taille = []
-
 for i in range(2):
     taille.append((int(input("choisissez votre résolution"))))
 
@@ -26,9 +25,15 @@ for i in range(nbr):
     x = taille[0] - (i * adj_coordone1)
     y = taille[1] - (i * adj_coordone2)
 
-    background.create_oval(
-        i * adj_coordone1, i * adj_coordone2, x, y, outline=color[0],
-        fill=color[0])
+    if taille[0] >= taille[1]:
+        background.create_oval(
+            i * adj_coordone2, i * adj_coordone2, y, y,
+            outline=color[0], fill=color[0])
+
+    elif taille[1] <= taille[0]:
+        background.create_oval(
+            i * adj_coordone1, i * adj_coordone2, x, y,
+            outline=color[0], fill=color[0])
 
     colorbis = color.pop(0)
     color.append(colorbis)
