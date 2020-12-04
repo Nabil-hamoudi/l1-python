a210c5859d061380d2b64af9e2a119e2f50e4bda
Nabil <nabil.hamoudi@ens.uvsq.fr>
2020-12-04 22:37:21 +0100

    Train

---
+++ exercises/TD04_listes copy/syracuse.py
@@ -0,0 +1,45 @@
+def syracuse(n):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    pass
+
+
+print(syracuse(3))
+
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
+    pass
+
+
+print(testeConjecture(10000))
+
+
+def tempsVol(n):
+    """ Retourne le temps de vol de n """
+    return len(syracuse(n))
+
+
+print("Le temps de vol de", 3, "est", tempsVol(3))
+
+
+def tempsVolListe(n_max):
+    """ Retourne la liste de tous les temps de vol de 1 à n_max """
+    pass
+
+
+print(tempsVolListe(100))
+
+
+def VerTempsDeVol(n_max):
+    pass
+
+
+print(VerTempsDeVol(10000))
+
+
+def syracuse2(n_max):
+    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
+    pass
+
+
+print(syracuse2(10000))
