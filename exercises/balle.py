fdb4c6f5d3c3e9e04819d6e2410cf4209f573523
Nabil HAMOUDI <nabil.hamoudi@ens.uvsq.fr>
2021-03-29 14:36:18 +0200

    Balle

--- exercises/balle.py
+++ exercises/balle.py
@@ -5,6 +5,8 @@ import tkinter as tk
 
 LARGEUR = 600
 HAUTEUR = 400
+TRAIT = HAUTEUR // 2
+count = 0
 
 
 ###################
@@ -13,30 +15,46 @@ HAUTEUR = 400
 def creer_balle():
     """Dessine un rond bleu et retourne son identifiant
      et les valeurs de déplacements dans une liste"""
-    x, y = LARGEUR // 2, HAUTEUR // 2
+    global TRAIT
+    x, y = LARGEUR // 2, (HAUTEUR / 1.5)
     dx, dy = 3, 5
+    lx, ly = 0, 1
     rayon = 20
     cercle = canvas.create_oval((x-rayon, y-rayon),
                                 (x+rayon, y+rayon),
                                 fill="blue")
-    return [cercle, dx, dy]
+    ligne = canvas.create_line(0, TRAIT, LARGEUR, TRAIT, fill="white")
+    return [cercle, dx, dy], [ligne, lx, ly]
 
 
 def mouvement():
     """Déplace la balle et ré-appelle la fonction avec un compte-à-rebours"""
+    global balle, ligne, TRAIT
     rebond()
     canvas.move(balle[0], balle[1], balle[2])
-    canvas.after(100, mouvement)
+    canvas.move(ligne[0], ligne[1], ligne[2])
+    TRAIT += 1
+    canvas.after(20, mouvement)
 
 
 def rebond():
     """Fait rebondir la balle sur les bords du canevas"""
-    global balle, HAUTEUR, LARGEUR
+    global balle, TRAIT, HAUTEUR, LARGEUR, ligne, count
     x0, y0, x1, y1 = canvas.coords(balle[0])
     if x0 <= 0 or x1 >= LARGEUR:
         balle[1] = -balle[1]
-    if y0 <= 0 or y1 >= HAUTEUR:
+        count += 1
+    if y1 >= HAUTEUR:
         balle[2] = -balle[2]
+        count += 1
+    elif y0 <= TRAIT:
+        canvas.move(ligne[0], ligne[1], -50)
+        balle[2] = -balle[2]
+        TRAIT -= 50
+        count += 1
+    if count == 30:
+        racine.destroy()
+
 
 
 ######################
@@ -45,6 +63,6 @@ def rebond():
 racine = tk.Tk()
 canvas = tk.Canvas(racine, bg="black", width=LARGEUR, height=HAUTEUR)
 canvas.grid()
-balle = creer_balle()
+balle, ligne = creer_balle()
 mouvement()
 racine.mainloop()
